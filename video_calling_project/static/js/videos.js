24289d714b1ec6d002520d2da7bd7010e5dda268
Shilu <shilu842003@gmail.com>
2022-05-11 16:31:25 +0530

    added search selection

--- video_calling_project/static/js/videos.js
+++ video_calling_project/static/js/videos.js
@@ -192,3 +192,7 @@ screen_size.addEventListener('click', changeScreenSize);
  }
 
  rate_icon.addEventListener('click', changeRate)
+
+
+const video11 =  document.getElementsByClassName("player_video viewer")
+video11.src = 'https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164'
