5591302b10e403fcb325412e6a7f256406f3a19a
Shilu <shilu842003@gmail.com>
2022-05-26 17:33:01 +0530

    added a test taking page

--- video_calling_project/test_page/views.py
+++ video_calling_project/test_page/views.py
@@ -31,45 +31,5 @@ def test_page(request) :
 def videos_page(request) :
     return render(request, 'test_page/view_videos.html')
 
-
-
-
-
-# tomorrow 
-
-# We need to add every bucket key into our csv file we need a different csv file and
-# object for each subject
-# eg :
-    # if subject = "english" -> need sepearate bucket and database 
-
-# it is even good, we have seperate bucket based on the department, subject. 
-
-# we need to stream the videos based on the request
-# eg :
-   # if user asks a subject english, for department- xyz.
-     # we need to stream that information only.
-    
-  # if user asks a subject tamil, for department - abc.
-    # we need to stream that videos only 
-
-# we will do this by querying the database and getting the keys, for specific subject
-# and class. and stream it in fronted. because of cloudfront we just need to change
-# the endpoints, which is filename(keys). 
-
-# so we will do it dynamically, by having a dropdown boxes for the subject and dept
-# whatever user chooses, we will stream those videos from the bucket.
-
-# we need to stream the video dynamic content. so it html pages, should not contain
-# anything. we should dynamically create the things.
-
-
-
-
-
-# tomorrow ------> ttry to connect the idea with data ingestion and try to get the 
-# csv file from the clients and try to create a bucket and everything according to
-# it.
-# because we neednot have to handle the file at the backend.
-# try to implement the code, to create the bucket automatically from the 
-# users input and use, it, in the cloudfront we can specify the folders and filename
-# to get the video, so we can just use it, for the easier purpose.
+def take_tst(request) :
+    return render(request, 'test_page/index.html')
